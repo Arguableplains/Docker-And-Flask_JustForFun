8b5bf19aa76386792704e66bbc909a98336edf6c
Rhian Rodrigues Barbosa <rhianbarbosa2003@gmail.com>
2023-08-07 11:40:51 -0300

    Remake of the PrincipalScript.sh file - Error messages and Aborting commands inserted

--- PrincipalScript.sh
+++ PrincipalScript.sh
@@ -5,14 +5,49 @@ echo "START"
 echo "Initializing Database Container and Configs"
 #Docker SQL commands
 sudo docker pull mysql:latest
+
 sudo docker run --name mysql-container -e MYSQL_ROOT_PASSORD=root -d mysql:latest
+#Docker SQL Command Verification - Creating
+if [ $? -ne 0 ]; then
+    sudo docker rm -f mysql-container
+    sudo docker image rm mysql:latest
+    echo "There was an error while creating and starting the container"
+    exit 1
+fi
+
 sudo docker exec -i mysql-container mysql -u root -p sys < ./DatabaseBooks.sql
+#Docker SQL Command Verification - Executing Script
+if [ $? -ne 0 ]; then
+    sudo docker rm -f mysql-container
+    sudo docker image rm mysql:latest
+    echo "There was an error while running the SQL script into the Database Container"
+    exit 1
+fi
+
+echo "The SQL Container creation was successful"
 
 echo "Initializing Main API Container"
 #Docker API commands
-sudo docker build -t the-sql-python-api-image .
-sudo docker run -p 9000:5000 --link mysql-container --name pythonapi -d the-sql-python-api-image
 
-echo "END"
+sudo docker build -t the-sql-python-api-image .
+#Docker SQL Command Verification - Executing Script
+if [ $? -ne 0 ]; then
+    sudo docker image rm the-sql-python-api-image
+    echo "There was an error while creating the API image"
+    exit 1
+fi
+
+sudo docker run -p 9000:5000 --link mysql-container --name pythonapi -d the-sql-python-api-image
+#Docker SQL Command Verification - Executing Script
+if [ $? -ne 0 ]; then
+    sudo docker rm -f pythonapi
+    sudo docker image rm the-sql-python-api-image
+    echo "There was an error while creating and starting the API Container"
+    exit 1
+fi
+
+echo "The API Container creation was successful"
+
+echo "END - Everything was successful"
 
 
